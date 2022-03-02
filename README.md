997f4ffe786d4699e6288f0f8ccc3663518a6b1f
Delphine Moutault <dlphn@hotmail.fr>
2022-03-02 15:28:40 +0100

    French langue put for indications passwd /calendar

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Cinema_Noyant
+
 Send email to spectators. Book slots from volunteers.
+
+# Technologies
+
+Node v16
+Express
+Vue.js 3
+Primevue
+
+# All features
+
+-   Connection of volunteer with email and password
+-   Input new subscriber from the administrator
+-   Send email to all subscribers with a image (movie poster) (only from administrator)
+-   Send email to all volunteers (only from administrator)
+-   Input new session (only from administrator)
+-   Give availabilities for each session from each volunteer
+-   Impute role for each volunteer if he/she is available (only from administrator)
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -17,10 +17,53 @@ import Password from "primevue/password";
 import "primevue/resources/themes/saga-orange/theme.css"; //theme
 import "primevue/resources/primevue.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
-import store from './store'
+import store from "./store";
 
-createApp(App).use(store)
-	.use(PrimeVue)
+createApp(App)
+	.use(store)
+	.use(PrimeVue, {
+		locale: {
+			weak: "Faible",
+			medium: "Moyen",
+			strong: "Fort",
+			passwordPrompt: "Saisir un mot de passe",
+			accept: "OK",
+			reject: "Non",
+			dayNames: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+			dayNamesShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+			dayNamesMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa"],
+			monthNames: [
+				"Janvier",
+				"Février",
+				"Mars",
+				"Avril",
+				"Mai",
+				"Juin",
+				"Juillet",
+				"Août",
+				"Septembre",
+				"Octobre",
+				"Novembre",
+				"Décembre",
+			],
+			monthNamesShort: [
+				"Jan",
+				"Fév",
+				"Mar",
+				"Avr",
+				"Mai",
+				"Jun",
+				"Jul",
+				"Aoû",
+				"Sep",
+				"Oct",
+				"Nov",
+				"Déc",
+			],
+			firstDayOfWeek: 1,
+			dateFormat: "dd/mm/yyyy",
+		},
+	})
 	.use(ConfirmationService)
 	.component("Dialog", Dialog)
 	.component("InputText", InputText)
